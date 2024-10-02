6cd2c5072f337258ee1c7a68ecd465ede4c180d1
Joshua McNeel <joshmcneel@gmail.com>
2024-10-02 13:28:25 -0400

    feat: added API to satisfy user stories

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -9,6 +10,12 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
+
+// Parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Parse application/json
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
@@ -21,4 +28,49 @@ app.get('/api/hello', function(req, res) {
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
+});
+
+// URL Shortening Logic
+const validUrl = require('valid-url');
+const urlDatabase = {};
+let urlCounter = 1;
+
+app.post('/api/shorturl', (req, res) => {
+  const originalUrl = req.body.url;
+
+  // Check if submitted URL is valid
+  if (!validUrl.isWebUri(originalUrl)) {
+    return res.json({ error: 'invalid url' });
+  }
+
+  // Check if URL already exists in the database
+  for (let key in urlDatabase) {
+    if (urlDatabasep[key].original_url === originalUrl) {
+      return res.json({
+        original_url: originalUrl,
+        short_url: key,
+      });
+    }
+  }
+
+  // If the URL is new, add it to the database and generate a short URL
+  const shortUrl = urlCounter++;
+  urlDatabase[shortUrl] = { original_url: originalUrl };
+
+  res.json({
+    original_url: originalUrl,
+    short_url: shortUrl,
+  });
+
+  // Redirect route
+  app.get('/api/shorturl/:shortUrl', (req, res) => {
+    const shortUrl = req.params.shortUrl;
+    const urlData = urlDatabase[shortUrl];
+
+    if (urlData) {
+      return res.redirect(urlData.original_url);
+    } else {
+      return res.json({ error: 'invalid url' });
+    }
+  });
 });
